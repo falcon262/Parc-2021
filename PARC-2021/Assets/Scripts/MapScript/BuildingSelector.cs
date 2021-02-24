28440726acfbb0a7a57a3fbc8276c02f7f6be9b2
Joseph Asante <thommpson19@gmail.com>
2021-02-24 13:59:18 +0000

    Added Map Menu

---
+++ PARC-2021/Assets/Scripts/MapScript/BuildingSelector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSelector : MonoBehaviour
+{
+    GameObject lastbuilding = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Selector();
+    }
+
+    void Selector()
+    {
+        RaycastHit hit;
+        Ray ray;
+        
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 300.0f))
+        {
+            
+            
+            if(hit.transform.gameObject.tag == "Building")
+            {
+                Color buildingColor = hit.transform.gameObject.GetComponent<MeshRenderer>().material.color;
+                lastbuilding = hit.transform.gameObject;
+                buildingColor.a = 1;
+                hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = buildingColor;
+            }
+            else if (hit.transform.gameObject.tag != "Building")
+            {
+                if (lastbuilding == null)
+                    return;
+                else
+                {
+                    Color buildingColor = lastbuilding.GetComponent<MeshRenderer>().material.color;
+                    buildingColor.a = 0.3f;
+                    lastbuilding.GetComponent<MeshRenderer>().material.color = buildingColor;
+                } 
+            }
+
+        }
+        
+    }
+}
---
+++ PARC-2021/Assets/Scripts/MapScript/MapCam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCam : MonoBehaviour
+{
+    public GameObject Senegal;
+
+    public void SpawnBuildings()
+    {
+        Senegal.SetActive(true);
+    }
+}
