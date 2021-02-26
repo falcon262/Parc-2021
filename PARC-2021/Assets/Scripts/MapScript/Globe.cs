9a89d3670c8e0440b49d70b3c098b30a7193b7ce
Joseph Asante <thommpson19@gmail.com>
2021-02-26 19:26:21 +0000

    MainMenuChanges

--- PARC-2021/Assets/Scripts/MapScript/Globe.cs
+++ PARC-2021/Assets/Scripts/MapScript/Globe.cs
@@ -14,6 +14,6 @@ public class Globe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, turnSpeed, 0);
+        transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
     }
 }
