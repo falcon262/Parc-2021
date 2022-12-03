84ab9d4753ba540d41e57985fd4ff7f65c1859a7
falcon262 <67340827+falcon262@users.noreply.github.com>
2022-12-03 16:46:12 +0000

    MethathonUpdate
    Removing Parc dependancies

--- PARC-2021/Assets/Scripts/F1.cs
+++ PARC-2021/Assets/Scripts/F1.cs
@@ -7,7 +7,7 @@ public class F1 : MonoBehaviour
     public Manager manager;
     private void OnTriggerEnter(Collider other)
     {
-        if (this.gameObject.tag == "F2" && other.gameObject.tag == "Robot")
+        if (this.gameObject.tag == "F1" && other.gameObject.tag == "Robot")
         {
             manager.score += 150;
             manager.ScoreText.text = manager.score.ToString();
